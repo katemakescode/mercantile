cc38117e4d4fa4704b745290ef6766c5ed361b27
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-11 14:23:25 +1300

    Place Webpack output in shared static directory

--- mercantile/frontend/src/index.js
+++ mercantile/frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {render} from 'react-dom';
 
 function Hello() {
-    return <h1 >Hello!</h1 >;
+    return <h1 >Hello from Webpack!</h1 >;
 }
 
 render(
     <Hello />,
     document.getElementById('react-root')
 );
+
+const btn = document.getElementById('click')
+btn.addEventListener('click', () => alert('You clicked the button!'))
