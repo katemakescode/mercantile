888c406c50009faf272443cf3f370286b61682b1
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-11 20:21:32 +1300

    Move warning section into React component

--- mercantile/frontend/src/index.js
+++ mercantile/frontend/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Button from 'react-bootstrap/Button';
 
-function Hello() {
-    return <h1 >Hello from React-Bootstrap!</h1 >;
+function Warning() {
+    return (<>
+        <div className="col col-md-9 mx-auto" >
+            <h3 >WARNING</h3 >
+            <h5 className="text my-4 px-1" >
+                This bookstore, despite appearances, sells no books.
+            </h5 >
+            <Button variant="outline-primary" id="click" className="mt-4" >Clickety</Button >
+        </div >
+    </>);
 }
 
 render(
-    <Hello />,
+    <Warning />,
     document.getElementById('react-root')
 );
 
