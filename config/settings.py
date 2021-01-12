3d172d6a866cc4ab35a71a547c4541ed94230e58
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-12 14:39:19 +1300

    Create books app

--- config/settings.py
+++ config/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'frontend.apps.FrontEndConfig',
+    'books.apps.BooksConfig',
     'rest_framework',
+    'frontend.apps.FrontEndConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
