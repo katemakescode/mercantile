74029ae45c3dbf63fb5a225bc9abc2f49e89fb89
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-13 12:12:34 +1300

    Create basic Book model

--- mercantile/books/models.py
+++ mercantile/books/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+
 class Book(models.Model):
-    pass
+    id = models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
