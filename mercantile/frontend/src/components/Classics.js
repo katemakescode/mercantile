d0450386cd1362e6260318863bc657ecd5f38a68
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-13 19:43:00 +1300

    Re-ordered scripts in base.html to ensure that jQuery loads before Bootstrap

--- mercantile/frontend/src/components/Classics.js
+++ mercantile/frontend/src/components/Classics.js
@@ -10,7 +10,7 @@ function BookCard({title}) {
   return (
       <div className="card col-md-2 mx-4 mx-md-4 my-4 pt-3" >
         <a href="https://en.wikipedia.org/wiki/Anna_Karenina" >
-          <div className="card-body m-2" >
+          <div className="card-body m-0" >
             <img className="card-img-top"
                  alt="Cover of the 2000 Modern Library edition of Anna Karenina by Leo Tolstoy"
                  src={window.static + '/mercantile/img/anna_karenina.jpg'} />
