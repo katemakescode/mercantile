609dda9f3b06b7dee478b4de85c5c992530c4288
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-14 16:25:54 +1300

    Add List and Detail API views for the Book model

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('frontend.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ mercantile/books/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+
+from .serializers import BookSerializer
+from ..models import Book
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
