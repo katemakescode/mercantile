81c88a1eec45de463c2c84d4674c38b3136c6eef
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-18 10:11:47 +1300

    Display fetched book data
    Issue accessing volumeInfo property

--- mercantile/frontend/src/components/BookSearch.js
+++ mercantile/frontend/src/components/BookSearch.js
@@ -19,33 +19,48 @@ function BookSearchForm() {
   );
 }
 
+function SearchBookCard({book}) {
+  return (
+      <div className="card col-md-2 mx-4 mx-md-4 my-4 pt-3" >
+        <a href="https://en.wikipedia.org/wiki/Anna_Karenina" >
+          <div className="card-body m-0" >
+            <img className="card-img-top"
+                 alt="Cover of the 2000 Modern Library edition of Anna Karenina by Leo Tolstoy"
+                 src={window.static + 'mercantile/img/anna_karenina.jpg'} />
+            <h5 className="card-title pt-3" >{book.etag}</h5 >
+            <p className="card-text" >{book.id}</p >
+          </div >
+        </a >
+      </div >
+  );
+}
+
 function BookSearchResultsList({query}) {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
+    // API key AIzaSyDh4Ha95ti7GLkVbPKdyXuoowEXOgRVHak
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
         .then(response => response.json())
-        .then(setData);
+        .then(response => setItems(response.items));
     setIsLoaded(true);
   }, [])
 
-  if (!data) {
-    // return <div>Error: {error.message}</div>;
-    return <div >Loading</div >;
-  } else {
-    return (
-        // books.map(book => <BookCard key={book.id} book={book} />)
-        <div >{JSON.stringify((data.items))}</div >
-    );
+  if (items) {
+    return (<>
+      {items.slice(0, 2).map(book => <SearchBookCard key={book.id} book={book} />)}
+    </>);
   }
 }
 
 export default function BookSearch() {
   return (
-      <Container fluid as="section" id="booksearch" className="py-5 bg-color-light text-center" >
-        <h2 className="my-4" >Books for Book Lovers</h2 >
+      <Container fluid as="section" id="booksearch" className="py-5 bg-color-light" >
+        <Row className="my-4 justify-content-center" >
+          <h3 >Books for Book Lovers</h3 >
+        </Row >
         <Row className="my-4 justify-content-center" >
           <Col md="auto" ><BookSearchForm /></Col >
         </Row >
