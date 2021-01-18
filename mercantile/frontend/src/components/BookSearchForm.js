869dde275ebd2c66583df497784207159eaeb585
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-19 12:26:21 +1300

    Update query value as the user types it

--- mercantile/frontend/src/components/BookSearchForm.js
+++ mercantile/frontend/src/components/BookSearchForm.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
 
 export default function BookSearchForm() {
+  const [query, setQuery] = useState('cats');
+
+  const handleSubmit = event => alert("foo " + query)
+
   return (
-      <InputGroup className="mb-3" >
-        <InputGroup.Prepend ><InputGroup.Checkbox /></InputGroup.Prepend >
-        <FormControl placeholder="Find a book" />
-        <InputGroup.Append >
-          <Button variant="primary" onClick={() => alert('Button clicked')}>Search</Button >
-        </InputGroup.Append >
-      </InputGroup >
+      <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-3" >
+          <FormControl type="text" placeholder="Find a book" onChange={event => setQuery(event.target.value)}/>
+          <InputGroup.Append >
+            <Button type="submit" variant="primary" >Search</Button >
+          </InputGroup.Append >
+        </InputGroup >
+      </Form >
   );
 }
