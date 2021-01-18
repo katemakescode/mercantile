dc388f4dfde764e9bd5d35a82ecd38143b780307
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-18 18:05:28 +1300

    Add inlined OrderLines to Orders section of the admin page

--- mercantile/orders/admin.py
+++ mercantile/orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLine
+
+
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderLineInline]
+
+
+admin.site.register(Order, OrderAdmin)
+
