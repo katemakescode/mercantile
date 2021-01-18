ddb39e8acc559fe0f538f415b73443cb448ce98d
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-18 14:26:55 +1300

    Add ordering and __str__ methods to Order and OrderLine

--- mercantile/orders/models.py
+++ mercantile/orders/models.py
@@ -5,8 +5,17 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     date_shipped = models.DateTimeField(blank=True, null=True)
 
+    class Meta:
+        ordering = ('-date_ordered',)
+
+    def __str__(self):
+        return self.date_ordered
+
 
 class OrderLine(models.Model):
     order = models.ForeignKey('Order', on_delete=models.CASCADE)
     book_id = models.CharField(max_length=20)
     quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.book_id
