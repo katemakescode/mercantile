d33a72a3f5ac9b77c298f7e0b07987074b6a472f
Kate McKenzie <kate.j.mckenzie@gmail.com>
2021-01-30 15:16:42 +1300

    Back out django-debug-toolbar for now

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,19 +1,8 @@
-import debug_toolbar
 import dj_database_url
-
 from .base import *
 
 DEBUG = True
-
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-MIDDLEWARE += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-INTERNAL_IPS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 DATABASES = {
--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,9 +8,3 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
-
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#         path('__debug__/', include(debug_toolbar.urls))
-#     ] + urlpatterns
